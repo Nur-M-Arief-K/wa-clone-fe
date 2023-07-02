79d710c40cee5220014d33071e61f2b2332d8f33
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-07-02 14:46:25 +0700

    create tailwindcss setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import "./index.css";
 function App() {
   return (
-    <div>
-      <h1>HELLOOOOO</h1>
+    <div className="dark">
+      <h1 className="text-red-500">HELLOOOOO</h1>
     </div>
   );
 }
