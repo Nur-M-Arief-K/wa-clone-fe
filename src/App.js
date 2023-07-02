a5a7421beeaae270a71df9c6cdb66ef0366dbb47
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-07-02 16:00:25 +0700

    create redux-persist setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./index.css";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
+
 function App() {
   return (
     <Router>
--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,32 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import createFilter from "redux-persist-transform-filter";
+
 import userSlice from "../features/user-slice";
+
+const saveUserOnlyFilter = createFilter("user", ["user"]);
+
+const persistConfig = {
+    key: "user",
+    storage,
+    whitelist: ["user"],
+    transforms: [saveUserOnlyFilter]
+};
 
 const rootReducer = combineReducers({
     user: userSlice
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
     devTools: true,
-})
+});
+
+export const persistor = persistStore(store);
