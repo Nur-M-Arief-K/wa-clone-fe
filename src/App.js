0c29516ff9a04af7dc9ee1eb00f6944943958b84
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-07-04 18:17:58 +0700

    add protected route

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import "./index.css";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
+
   return (
     <div className="dark">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/register" element={<Register />}></Route>
+          <Route
+            exact
+            path="/"
+            element={token ? <Home /> : <Navigate to="/login" />}
+          ></Route>
+          <Route
+            exact
+            path="/login"
+            element={!token ? <Login /> : <Navigate to="/" />}
+          ></Route>
+          <Route
+            exact
+            path="/register"
+            element={!token ? <Register /> : <Navigate to="/" />}
+          ></Route>
         </Routes>
       </Router>
     </div>
